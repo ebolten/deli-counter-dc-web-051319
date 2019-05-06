dd00d800f7e82f9f61f2e473b20311224365d24b
Emily Bolten <emilybolten@gmail.com>
2019-05-06 16:43:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+# Write your code here
+
+def take_a_number(lineArray,name)
+
+    lineArray.push(name);
+    sum = lineArray.length
+    puts "Welcome, #{name}. You are number #{sum} in line."
+
+    return lineArray
+  end
+
+def now_serving(currentLine)
+
+  if (currentLine.length == 0)
+    puts "There is nobody waiting to be served!"
+
+  else
+    puts "Currently serving #{currentLine[0]}."
+    currentLine.shift
+    return currentLine
+  end
+
+    currentLine.shift
+    return currentLine
+  end
+
+
+def line(currentLine)
+
+    if currentLine.length == 0
+      puts "The line is currently empty."
+    end
+
+    lineString = "This line is currently: "
+    lineArray = []
+
+    if currentLine.length > 0
+      for i in 0..currentLine.length - 1
+        lineArray.push("#{i + 1}. #{currentLine[i]}")
+      end
+      print "#{lineString} #{lineArray} "
+      return currentLine
+    end
+  end
